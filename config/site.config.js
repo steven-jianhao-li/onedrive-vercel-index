636e5b52285851e1068b68cb829eb1160d96a87b
Jianhao Li <61460116+steven-jianhao-li@users.noreply.github.com>
2024-12-10 16:29:06 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,8 +42,8 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/课件', '/录屏', '/', '/软件', '/Pictures'],
-  // protectedRoutes: ['/课件', '/录屏', '/软件'],
+  // protectedRoutes: ['/课件', '/录屏', '/', '/软件', '/Pictures'],
+  protectedRoutes: ['/课件', '/录屏', '/软件'],
   
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:spencer.wushangbo@gmail.com',
